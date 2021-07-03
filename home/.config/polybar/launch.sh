65a639329f9c7b6d6c8d833faf797f7ff2c3ae36
Don Petersen <don@donpetersen.net>
2021-07-03 14:11:25 -0700

    New video card, new configs
    Still hardcoded...

--- home/.config/polybar/launch.sh
+++ home/.config/polybar/launch.sh
@@ -6,7 +6,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-m=$(xrandr --query | grep " connected" | grep primary | cut -d" " -f1)
+m=DP-2
+# I'm not sure why this stopped working after I got a new video card. The
+# output of this command when I run it is correct. Shrug. I'll deal with it
+# when I have to make this config work on multiple machines...
+#m=$(xrandr --query | grep " connected" | grep primary | cut -d" " -f1)
 cmd=(env "MONITOR=$m" polybar --reload main)
 
 if [[ $# -gt 0 ]] && [[ $1 = "block" ]]; then
