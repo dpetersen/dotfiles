e1edbd5ae73e11e1740e3777ee86c615b74483ac
Don Petersen <don@initialcapacity.io>
2025-01-07 15:44:27 -0800

    jj completion in fish

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -191,6 +191,8 @@ end
 
 export RIPGREP_CONFIG_PATH=$HOME/.ripgreprc
 
+jj util completion fish | source
+
 # Some environments I'm using are setting PAGER to something dumb, like `less`
 # with no arguments so it's launching less for things that are 2 lines long.
 if set -q PAGER
