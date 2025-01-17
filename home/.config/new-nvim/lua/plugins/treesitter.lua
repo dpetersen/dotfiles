de77373163728fd7597b1a8e1fed401eb85e6b8b
Don Petersen <don@initialcapacity.io>
2025-01-17 01:12:21 +0000

    Ensure treesitter updates correctly

--- home/.config/new-nvim/lua/plugins/treesitter.lua
+++ home/.config/new-nvim/lua/plugins/treesitter.lua
@@ -1,21 +1,23 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		main = "nvim-treesitter.configs",
-		dependencies = {
-			-- A treesitter-aware version of matchit. Can work without treesitter but it's
-			-- significantly faster with treesitter enabled in large files.
-			"andymass/vim-matchup",
-			-- Just a treesitter-aware port of tpope/endwise that works in a few languages
-			"RRethy/nvim-treesitter-endwise",
-		},
-		opts = {
-			auto_install = true,
-			highlight = {
-				enable = true,
-			},
-			endwise = { enable = true },
-			matchup = { enable = true },
-		},
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        main = "nvim-treesitter.configs",
+        dependencies = {
+            -- A treesitter-aware version of matchit. Can work without treesitter but it's
+            -- significantly faster with treesitter enabled in large files.
+            "andymass/vim-matchup",
+            -- Just a treesitter-aware port of tpope/endwise that works in a few languages
+            "RRethy/nvim-treesitter-endwise",
+        },
+        opts = {
+            auto_install = true,
+            highlight = {
+                enable = true,
+            },
+            endwise = { enable = true },
+            matchup = { enable = true },
+        },
+        -- Ensure that everytime the plugin is updated, this command is re-run
+        build = ":TSUpdate"
+    },
 }
