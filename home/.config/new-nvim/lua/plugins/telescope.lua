22fd8839404fc69d5b4a2130ac57ff1c2bf89d27
Don Petersen <don@initialcapacity.io>
2025-01-24 01:37:28 +0000

    Fix my find-in-config telescope mapping
    The grep one is still failing, but I'm closer to finding out why

--- home/.config/new-nvim/lua/plugins/telescope.lua
+++ home/.config/new-nvim/lua/plugins/telescope.lua
@@ -13,19 +13,21 @@ return {
 				["t"] = telescope.treesitter,
 				["s"] = telescope.lsp_document_symbols,
 				["S"] = telescope.lsp_dynamic_workspace_symbols,
-				-- TODO: these aren't working yet, but seems like they should and they would be useful
 				["nf"] = function()
 					telescope.find_files({
 						cwd = vim.fn.stdpath("config"),
 						prompt_title = "Neovim Config",
+						-- Needs to follow symlinks because my configs are symlinked by homeshick!
+						follow = true,
 						hidden = true,
 					})
 				end,
+				-- TODO: This isn't functioning yet, but I'm guessing it's because of homeshick and symlinks.
+				-- Probably need to follow the symlink to its source?
 				["ng"] = function()
 					telescope.live_grep({
 						cwd = vim.fn.stdpath("config"),
 						prompt_title = "Neovim Config",
-						hidden = true,
 					})
 				end,
 			}
