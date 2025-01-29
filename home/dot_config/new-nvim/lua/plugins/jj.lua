d98a51c16f0790f46c1b646c93ae264a43938ead
Don Petersen <don@initialcapacity.io>
2025-01-29 19:41:01 +0000

    Bring in jj-diffconflicts

--- home/dot_config/new-nvim/lua/plugins/jj.lua
+++ home/dot_config/new-nvim/lua/plugins/jj.lua
@@ -10,4 +10,11 @@ return {
 			require("hunk").setup()
 		end,
 	},
+	{
+		-- A conflict resolution editor for Jujutsu conflicts. Requires
+		-- configuration in jj to actually use this tool, as described in:
+		-- https://github.com/rafikdraoui/jj-diffconflicts#invoking-through-jj-resolve
+		"rafikdraoui/jj-diffconflicts",
+		-- Doesn't seem to need setup called
+	},
 }
