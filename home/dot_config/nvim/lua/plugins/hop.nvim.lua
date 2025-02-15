5f819dcc224b3cd445219976d341cb1c8a298477
Don Petersen <don@initialcapacity.io>
2025-02-15 23:17:03 +0000

    Add which-key with some initial configuration of my custom maps

--- home/dot_config/nvim/lua/plugins/hop.nvim.lua
+++ home/dot_config/nvim/lua/plugins/hop.nvim.lua
@@ -11,16 +11,16 @@ return {
 			-- All the mappings have a lower-case and upper-case version to denote single/multi-window
 
 			-- HopWord
-			vim.keymap.set("n", "<Leader>hw", hop.hint_words, { noremap = true })
+			vim.keymap.set("n", "<Leader>hw", hop.hint_words, { noremap = true, desc = "Hop to word" })
 			vim.keymap.set("n", "<Leader>hW", function()
 				hop.hint_words({ multi_windows = true })
-			end, { noremap = true })
+			end, { noremap = true, desc = "Hop to word (multi-window)" })
 
 			-- HopPattern
-			vim.keymap.set("n", "<Leader>h/", hop.hint_patterns, { noremap = true })
+			vim.keymap.set("n", "<Leader>h/", hop.hint_patterns, { noremap = true, desc = "Hop to pattern" })
 			vim.keymap.set("n", "<Leader>h?", function()
 				hop.hint_patterns({ multi_windows = true })
-			end, { noremap = true })
+			end, { noremap = true, desc = "Hop to pattern (multi-window)" })
 		end,
 	},
 }
---
+++ home/dot_config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,15 @@
+return {
+	-- Context aware help that shows your keymaps, to help learn new ones or reinforce old ones.
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+	},
+}
