9f5634f63c8e9df5240c9d89dd6231ba6a7587a5
Don Petersen <donald.petersen@datavant.com>
2025-03-19 16:12:31 -0700

    OSX install script for general tools
    Not sure what to do about kubectx, stern, etc, which aren't required
    everywhere.

---
+++ install-osx.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# node: Nvim copilot.lua needs this
+brew install \
+        node \
+        neovim \
+        jj \
+        fzf \
+        starship \
+        atuin \
+        tmux \
+        direnv \
+        asdf \
+        eza \
+        jq \
+        jid \
+        go \
+        rust \
+        topgrade \
+        delta \
+        ripgrep \
+        fd \
+        bat
