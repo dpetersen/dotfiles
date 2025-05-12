28426301be6d7dbb3d91bcf2dccd3a64e17910a6
Don Petersen <donald.petersen@datavant.com>
2025-05-12 11:51:37 -0700

    Keep unimpaired for now, but add a TODO about it

--- home/dot_config/nvim/lua/plugins/vim-unimpaired.lua
+++ home/dot_config/nvim/lua/plugins/vim-unimpaired.lua
@@ -1,1 +1,5 @@
+-- TODO: It's possible I can stop using unimpaired? A bunch of the mappings from
+-- it were folded into Neovim in 0.11. See if you still use anything from it:
+--
+-- https://gpanders.com/blog/whats-new-in-neovim-0-11/#more-default-mappings
 return { { "tpope/vim-unimpaired" } }
