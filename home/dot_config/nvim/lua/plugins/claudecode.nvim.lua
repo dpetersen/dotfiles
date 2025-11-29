207635d2d4bbd02d69a6d4f12d07410aaf5ae7a2
Don Petersen <donald.petersen@datavant.com>
2025-11-28 18:16:15 -0800

    Update neovim plugins
    - Remove avante.nvim (not using it)
    - Add claudecode.nvim for Claude Code integration
    - Add poetry-nvim for Python virtualenv detection
    - Update lazy-lock.json with current plugin versions

---
+++ home/dot_config/nvim/lua/plugins/claudecode.nvim.lua
@@ -0,0 +1,29 @@
+return {
+	"coder/claudecode.nvim",
+	dependencies = { "folke/snacks.nvim" },
+	config = true,
+	opts = {
+		-- And this is why we don't use Node for CLIs. Somehow I have multiple Claudes
+		-- depending on what frontend stuff I was doing. This is the right one, I guess?
+		-- This is suggested in the plugin README.
+		terminal_cmd = "~/.claude/local/claude",
+	},
+	keys = {
+		{ "<leader>a", nil, desc = "AI/Claude Code" },
+		{ "<leader>ac", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
+		{ "<leader>af", "<cmd>ClaudeCodeFocus<cr>", desc = "Focus Claude" },
+		{ "<leader>ar", "<cmd>ClaudeCode --resume<cr>", desc = "Resume Claude" },
+		{ "<leader>aC", "<cmd>ClaudeCode --continue<cr>", desc = "Continue Claude" },
+		{ "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
+		{ "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
+		{
+			"<leader>as",
+			"<cmd>ClaudeCodeTreeAdd<cr>",
+			desc = "Add file",
+			ft = { "NvimTree", "neo-tree", "oil", "minifiles" },
+		},
+		-- Diff management
+		{ "<leader>aa", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
+		{ "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
+	},
+}
---
+++ home/dot_config/nvim/lua/plugins/python.lua
@@ -0,0 +1,7 @@
+return {
+	-- Super simple plugin that looks for poetry.lock files in your root
+	-- and sets the VIRTUAL_ENV appropriately. Makes it so I don't have
+	-- to `poetry run nvim` all the time.
+	"karloskar/poetry-nvim",
+	opts = {},
+}
