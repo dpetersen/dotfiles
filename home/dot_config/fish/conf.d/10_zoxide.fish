f9c3b9dbcccab82b37010cfcf00a220f2ed4da3e
Don Petersen <don@donpetersen.net>
2025-11-29 15:52:02 -0800

    Add zoxide fzf keybindings for directory navigation
    Ctrl-j opens zoxide's interactive picker (fzf sorted by frecency).
    Ctrl-k does the same but scoped to directories under cwd.

--- home/dot_config/fish/conf.d/10_zoxide.fish
+++ home/dot_config/fish/conf.d/10_zoxide.fish
@@ -1,3 +1,32 @@
 if status is-interactive
     zoxide init fish | source
+
+    # Ctrl-j: open zoxide's interactive directory picker (fzf sorted by frecency).
+    # Wraps __zoxide_zi to properly clear/restore the command line and repaint.
+    function __zoxide_zi_widget
+        set -l buf (commandline)
+        commandline -r ''
+        __zoxide_zi
+        commandline -r $buf
+        commandline -f repaint
+    end
+
+    bind \cj __zoxide_zi_widget
+    bind -M insert \cj __zoxide_zi_widget
+
+    # Ctrl-k: like Ctrl-j but scoped to directories under cwd.
+    # You'd think this was built into zoxide, but its queries aren't very fuzzy.
+    function __zoxide_zi_local_widget
+        set -l buf (commandline)
+        commandline -r ''
+        set -l result (zoxide query --list | grep "^$(pwd)/" | fzf)
+        if test -n "$result"
+            cd "$result"
+        end
+        commandline -r $buf
+        commandline -f repaint
+    end
+
+    bind \ck __zoxide_zi_local_widget
+    bind -M insert \ck __zoxide_zi_local_widget
 end
