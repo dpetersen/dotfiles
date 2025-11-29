88a956c1ab8c32ab636fcba7b1a209a63401ae05
Don Petersen <don@donpetersen.net>
2025-11-28 19:45:36 -0800

    Fix tmux auto-start for both Linux TTY and macOS
    Skip auto-start on raw TTY ($TERM=linux) to avoid nesting when starting
    Hyprland. Check for GHOSTTY_RESOURCES_DIR to work on macOS where DISPLAY
    isn't set.

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -185,11 +185,16 @@ end
 
 eval (starship init fish)
 
-# if status --is-interactive
-# and not set -q TMUX
-# and set -q DISPLAY
-#   exec tmux
-# end
+# Auto-start tmux for interactive graphical terminal sessions
+if status --is-interactive
+    and not set -q TMUX
+    # $TERM is "linux" on a raw TTY before starting a window manager - skip
+    # there to avoid launching tmux before Hyprland and nesting everything
+    and not string match -q "linux" $TERM
+    # Need either X11 ($DISPLAY) or a known terminal emulator
+    and test -n "$DISPLAY" -o -n "$GHOSTTY_RESOURCES_DIR"
+    exec tmux
+end
 
 export RIPGREP_CONFIG_PATH=$HOME/.ripgreprc
 
